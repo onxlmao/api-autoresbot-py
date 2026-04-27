f2e2f9be3b94546294e1c683c9b41252b810a8ad
Rico Ardiansyah <ricoardiansyahpersional@gmail.com>
2026-04-28 00:17:41 +0700

    Create __init__.py

---
+++ api_autoresbot/__init__.py
@@ -0,0 +1,11 @@
+"""
+Autoresbot API Client
+A Python client for the Autoresbot API
+"""
+
+from .api import ApiAutoresbot
+
+__version__ = "1.0.0"
+__author__ = "autoresbot"
+__email__ = "autoresbot@example.com"
+__all__ = ["ApiAutoresbot"]
